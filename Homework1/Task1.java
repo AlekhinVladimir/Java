428b0f98d33a7d7165fd5337b286eb5973c21cb8
AlekhinVladimir <Alehin.v.m@mail.com>
2023-03-28 16:40:11 +0400

    Task1

--- Homework1/Task1.java
+++ Homework1/Task1.java
@@ -3,3 +3,21 @@
 // Треугольное число 1 + 2 + 3 + 4 + 5 = 15
 // n! 1 * 2 * 3 * 4 * 5 = 120
 
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число n: ");
+        int n = iScanner.nextInt();
+        System.out.println(n);
+        int triangle = (n * (n + 1)) / 2;
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) 
+        {
+            factorial *= i;
+        }
+        System.out.println("Сумма чисел от 1 до " + n + " равна " + triangle);
+        System.out.println("Произведение чисел от 1 до " + n + " равно " + factorial);
+    }
+}
